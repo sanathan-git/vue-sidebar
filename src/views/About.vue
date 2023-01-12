73779cdff1197cbb24b5a9c985a8d5478216d665
unknown <{sanathan6522@gmail.com}>
2023-01-11 22:46:18 -0800

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+    <main class="about-page">
+        <h1>About</h1>
+        <p>This is the About page</p>
+    </main>
+</template>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <main class="home-page">
+        <h1>Home</h1>
+        <p>This is the home page</p>
+    </main>
+</template>
+
